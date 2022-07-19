48b63110bbd011172ef47bf3353f9d8fd15506da
Gyubin <gbcompany27@gmail.com>
2022-07-19 20:08:44 +0900

    secure update

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGO_URI: 'mongodb+srv://oruum:banana1004@oruum.bsopcnz.mongodb.net/oruum?retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const { User } = require('./models/User');
 const {userRouter} = require('./routes/userRoute');
-
-const MONGO_URI = 'mongodb+srv://oruum:banana1004@oruum.bsopcnz.mongodb.net/oruum?retryWrites=true&w=majority'
+const key = require('./config/key');
 
 const server = async() => {
     try {
-        await mongoose.connect(MONGO_URI);
+        await mongoose.connect(key.MONGO_URI);
         console.log('MongoDB connected!');
         app.use(express.json());
         
